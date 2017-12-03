6b8c75ee8578f1b7ac479ffc40c2d795b275e258
Nahu <nahueldolores@hotmail.com>
2017-12-03 20:22:43 -0300

    New website in Java using Spark!

---
+++ src/main/java/com/coalesce/website/Main.java
@@ -0,0 +1,39 @@
+package com.coalesce.website;
+
+import spark.Spark;
+
+import java.net.URL;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+import static spark.Spark.*;
+
+public class Main {
+
+    public Main() {
+        staticFileLocation("/public");
+
+        get("/", (request, response) -> renderIndex());
+
+        Spark.init();
+    }
+
+    private String renderIndex() {
+        try {
+            URL url = Main.class.getResource("/public/index.html");
+            System.out.println(url);
+            Path path = Paths.get(url.toURI());
+            return new String(Files.readAllBytes(path), Charset.defaultCharset());
+        } catch (Exception exception) {
+            exception.printStackTrace();
+        }
+        return "FATAL ERROR";
+    }
+
+    public static void main(String[] args) {
+        new Main();
+    }
+
+}
